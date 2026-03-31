aeee04c3f741bc1c76d19ef3a9d257add107fba0
France Dev <france.dev@outlook.com>
2026-03-31 00:30:04 -0300

    refactor: switch database adapter from PrismaPg to PrismaBetterSqlite3 in prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "../../prisma/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaPg } from "@prisma/adapter-pg"; // Postgres
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"; // Better SQLite3
 import "dotenv/config";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
+// const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaBetterSqlite3({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
